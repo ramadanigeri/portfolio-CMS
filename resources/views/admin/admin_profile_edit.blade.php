37a09f72394d83e91d1fd6a2e239c1e121699711
Geri Ramadani <gramadan@estee.com>
2023-04-03 15:53:10 +0200

    Update Footer, Implement Jquery image upload

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 <div class="page-content">
     <div class="container-fluid">
         <div class="row">
@@ -33,14 +33,14 @@
                             <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                                 <div class="col-sm-10">
-                                    <input class="form-control" name="profile_image" type="file" id="example-text-input">
+                                    <input class="form-control" name="profile_image" type="file" id="image">
                                 </div>
                             </div>
                             <!-- end row -->
                             <div class="row mb-3">
                                 <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                                 <div class="col-sm-10">
-                                    <img class="rounded avatar-lg" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
+                                    <img id="showImage" class="rounded avatar-lg" src="{{asset('backend/assets/images/small/img-5.jpg')}}" alt="Card image cap">
                                 </div>
                             </div>
                             <!-- end row -->
@@ -53,5 +53,17 @@
         <!-- end row -->
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 
 @endsection
--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> ©ramadanigeri</a>.
+                <script>document.write(new Date().getFullYear())</script> <a style="text-decoration:none" target="_blank" href="https://github.com/ramadanigeri">©ramadanigeri</a>.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Created by <a style="text-decoration:none"href="https://github.com/ramadanigeri">Geri Ramadani</a>
+                    Created by Geri Ramadani</a>
                 </div>
             </div>
         </div>
