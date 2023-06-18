e74b028eda720f2144ba3ca1a2b2b65c0cb43ecf
Geri Ramadani <gramadan@estee.com>
2023-06-18 15:01:56 +0200

    Add form validation

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -129,6 +129,9 @@
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
         <script src="{{ asset('backend/assets/js/code.js') }}"></script>
+
+        <script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
+        
     </body>
 
 </html>
